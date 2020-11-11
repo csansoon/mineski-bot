3fc3f185e4e1e5c88e7fe5e32734d8e60450a0da
Carlos Sansón <carlos.sanson@est.fib.upc.edu>
2020-11-11 15:20:12 +0100

    updated TOKEN and Procfile

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const { prefix, token, min_cooldown, locationsChannelId, locationsMessagesId } =
 const Discord = require('discord.js');
 const client = new Discord.Client({partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 client.commands = new Discord.Collection();
-client.login(token);
+//client.login(token);
+client.login(process.env.TOKEN);
 
 // CARGAR COMANDOS
 const fs = require('fs');
